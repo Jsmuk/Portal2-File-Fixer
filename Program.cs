0cb677f0631755f5ddfb2a293670901bd2c9e90e
Jsm <Jsm123@gmail.com>
2011-04-22 21:49:58 +0100

    First commit

---
+++ Program.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+using System.Diagnostics;
+
+namespace Fixer
+{
+    class Program
+    {
+        static byte NewVersion = 4;
+        static byte OldVersion = 5;
+        static string[] files;
+        static void Main(string[] args)
+        {
+            Console.Title = "Portal 2 File Fixer";
+            Console.WriteLine("Portal 2 File Fixer (By Jsm)");
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("DISCLAIMER:\nThis may break your texture files. This may not be a real issue (Just re extract the files) however I am not responsible for any damage that this program may cause");
+            Console.ForegroundColor = ConsoleColor.Gray;
+            Console.WriteLine("Enter the FULL path to where you have extracted your Portal 2 materials");
+            string folderPath = Console.ReadLine();
+            if (!buildFileArray(folderPath))
+            {
+                Console.WriteLine();
+                Console.WriteLine("Press any key to close");
+                Console.ReadKey();
+                Environment.Exit(-1);
+            }
+            makeAllChanges();
+            Console.WriteLine("The textures should now work in gmod (and possibly other source engine games that do not support the new 7.5 VTF format");
+            Console.WriteLine("Press any key to close");
+            Console.ReadKey();
+        }
+        static bool buildFileArray(string folder)
+        {
+            if (Directory.Exists(folder))
+            {
+                Stopwatch fileArraySw = new Stopwatch();
+                fileArraySw.Start();
+                files = Directory.GetFiles(folder, "*.vtf", SearchOption.AllDirectories);
+                fileArraySw.Stop();
+                Console.WriteLine("Done! - Found "+files.Length+" VTF files in "+fileArraySw.ElapsedMilliseconds+"ms");
+                Console.WriteLine("Ready to make changes");
+                return true;
+            }
+            else
+            {
+                Console.WriteLine("Error: Not a real folder");
+                return false;
+            }
+        }
+        static void makeAllChanges()
+        {
+            int i = 0;
+            Stopwatch changesSW = new Stopwatch();
+            changesSW.Start();
+            while (i < files.Length)
+            {
+                changeFile(files[i]);
+                i++;
+            }
+            changesSW.Stop();
+            Console.WriteLine("Completely done! - Modified " + i + "("+files.Length+") files in " + changesSW.ElapsedMilliseconds + "ms");
+        }
+        static void changeFile(string file)
+        {
+            BinaryReader br = new BinaryReader(File.Open(file, FileMode.Open, FileAccess.ReadWrite));
+            br.BaseStream.Seek(8, 0); // Seek to the 8th byte because the sub version is here
+            byte version = br.ReadByte();
+            if (version == OldVersion)
+            {
+                br.BaseStream.Seek(8, 0);
+                br.BaseStream.WriteByte(NewVersion);
+                br.BaseStream.Seek(8, 0);
+                if (br.ReadByte() == NewVersion)
+                {
+                    //Console.WriteLine("Fixed: " + file);
+                }
+                else
+                {
+                    Console.WriteLine("Oops I appear to have broken " + file);
+                }
+                br.BaseStream.Close();
+            }
+            else if (version == NewVersion)
+            {
+                //Console.WriteLine("Error: No need to fix " + file);
+            }
+            else
+            {
+                Console.WriteLine("Error: Cannot fix " + file);
+            }
+        }
+    }
+}
---
+++ Readme.txt
@@ -0,0 +1,49 @@
+﻿Portal File Fixer 
+----------------------------
+
+This is a really small and simple to use.
+
+Step by step guide to getting the portal 2 models working in gmod (skip the first bit if you have already done it)
+
+Firstly extract the models and materials from the Portal 2 VPK file into a gmod addon folder (Create an info.txt for it)
+Run the batch file mentioned in this thread http://www.facepunch.com/threads/642683-Step-by-Step-for-adding-L4D-models-to-Gmod 
+It must be run from INSIDE of your models folder 
+
+Once you have run that you can run this program, the instructions are quite simple. Simply paste in the path to your Portal 2 
+materials folder 
+(For example C:\Program Files (x86)\steam\steamapps\Username\garrysmod\garrysmod\addons\Portal2\materials)
+If you do not go directly to the materials folder it may take a long time (Just doing my portal2 folder took me 5 mins as opposed 
+to 30 seconds for just the material folder)
+
+Once you have told the program the folder you want it to do, just sit back and wait. Once its done it will tell you.
+
+-------------------------
+
+Credit
+
+Credit really should go to both "Matsilagi" and "ben1066" as they pointed out which value needed changing in the files. I simply
+made this small program to automate it. 
+
+-------------------------
+
+License 
+
+Copyright (C) 2011 by James "Jsm" McCartney
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE.
